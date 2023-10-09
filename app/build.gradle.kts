78c01fec2e05a4c70ef6cf6f971f4e30b61d1530
XY0797 <eq_software@foxmail.com>
2023-10-09 22:52:27 +0800

    release v2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.eq.ecjtuswlanautologin"
         minSdk = 24
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.5"
+        versionCode = 3
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
